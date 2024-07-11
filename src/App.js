512b26e84e05e306a449dc0a241e22aae3c88ef1
Swastikataria <145952059+Swastikataria@users.noreply.github.com>
2024-07-11 22:15:58 +0530

    Adding image to README

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@
 import React from 'react';
 import DialogflowChat from './components/DialogflowChat';
 import './index.css';
-import logo from './logo.png'; // Ensure you have a logo.png in the src directory
+import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
       <header>
         <img src={logo} alt="FinHelp Logo" />
-        <h1>FinMan</h1>
+        <h1>FinHelp</h1>
       </header>
       <div className="chat-container">
         <DialogflowChat />
